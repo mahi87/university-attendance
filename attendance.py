aeaef72c663fb133c09d8e46fb260d8b712b0394
Mahima choudhary <mahima.choudhary@jda.com>
2023-04-17 18:29:59 +0530

    format using black

--- attendance.py
+++ attendance.py
@@ -1,41 +1,44 @@
 MAX_CONSECUTIVE_LEAVES_ALLOWED = 4
 
-class Attendance():
-    def _is_not_valid(self,no_of_academic_days):
-        return no_of_academic_days<1
-    
+
+class Attendance:
+    def _is_not_valid(self, no_of_academic_days):
+        return no_of_academic_days < 1
+
     def __init__(self, no_of_academic_days):
         if self._is_not_valid(no_of_academic_days):
             raise ValueError("Invalid Input")
         self.no_of_academic_days = no_of_academic_days
         self.cache = {}
-        
+
     def _number_of_ways(self, days_to_fill, consecutive_leaves_consumed):
-        if (days_to_fill,consecutive_leaves_consumed) in self.cache:
-            return self.cache[(days_to_fill,consecutive_leaves_consumed)]
-        
+        if (days_to_fill, consecutive_leaves_consumed) in self.cache:
+            return self.cache[(days_to_fill, consecutive_leaves_consumed)]
+
         if consecutive_leaves_consumed == MAX_CONSECUTIVE_LEAVES_ALLOWED:
-            self.cache[(days_to_fill,consecutive_leaves_consumed)] = 0
+            self.cache[(days_to_fill, consecutive_leaves_consumed)] = 0
             return 0
-        
+
         if days_to_fill == 0:
-            self.cache[(days_to_fill,consecutive_leaves_consumed)] = 1
+            self.cache[(days_to_fill, consecutive_leaves_consumed)] = 1
             return 1
-        
-        res = self._number_of_ways(days_to_fill-1,0) + self._number_of_ways(days_to_fill-1,consecutive_leaves_consumed+1)
+
+        res = self._number_of_ways(days_to_fill - 1, 0) + self._number_of_ways(
+            days_to_fill - 1, consecutive_leaves_consumed + 1
+        )
         self.cache[(days_to_fill, consecutive_leaves_consumed)] = res
         return res
-    
+
     def ways_to_attend_classes(self):
         days_to_fill = self.no_of_academic_days
         consecutive_leaves_consumed = 0
         return self._number_of_ways(days_to_fill, consecutive_leaves_consumed)
-    
+
     def ways_to_miss_last_day(self):
         days_to_fill = self.no_of_academic_days - 1
         consecutive_leaves_consumed = 1
         return self._number_of_ways(days_to_fill, consecutive_leaves_consumed)
-    
+
     def probability_to_miss_grad_ceremony(self):
         miss_last_day = self.ways_to_miss_last_day()
         total_ways_to_attend_classes = self.ways_to_attend_classes()
--- test_attendance.py
+++ test_attendance.py
@@ -1,18 +1,20 @@
 import unittest
 from attendance import Attendance
 
+
 class TestWaysToAttendClasses(unittest.TestCase):
     def test_should_return_number_of_ways_to_attend_classes_over_5_days(self):
         no_of_academic_days = 5
         expected_no_of_ways = 29
         at = Attendance(no_of_academic_days)
         self.assertEqual(at.ways_to_attend_classes(), expected_no_of_ways)
-        
+
     def test_should_return_number_of_ways_to_attend_classes_over_10_days(self):
         no_of_academic_days = 10
         expected_no_of_ways = 773
         at = Attendance(no_of_academic_days)
         self.assertEqual(at.ways_to_attend_classes(), expected_no_of_ways)
+
 
 class TestWaysToMissLastDay(unittest.TestCase):
     def test_should_return_number_of_ways_to_miss_last_day_of_classes_for_5_days(self):
@@ -20,31 +22,37 @@ class TestWaysToMissLastDay(unittest.TestCase):
         expected_no_of_ways = 14
         at = Attendance(no_of_academic_days)
         self.assertEqual(at.ways_to_miss_last_day(), expected_no_of_ways)
-    
+
     def test_should_return_number_of_ways_to_miss_last_day_of_classes_for_10_days(self):
         no_of_academic_days = 10
         expected_no_of_ways = 372
         at = Attendance(no_of_academic_days)
         self.assertEqual(at.ways_to_miss_last_day(), expected_no_of_ways)
 
-class TestProbabilityToMissGradCeremony(unittest.TestCase):        
-    def test_should_return_probability_to_miss_grad_ceremony_in_string_format_for_5_days(self):
+
+class TestProbabilityToMissGradCeremony(unittest.TestCase):
+    def test_should_return_probability_to_miss_grad_ceremony_in_string_format_for_5_days(
+        self,
+    ):
         no_of_academic_days = 5
         expected_probability = "14/29"
         at = Attendance(no_of_academic_days)
         self.assertEqual(at.probability_to_miss_grad_ceremony(), expected_probability)
-        
-    def test_should_return_probability_to_miss_grad_ceremony_in_string_format_for_10_days(self):
+
+    def test_should_return_probability_to_miss_grad_ceremony_in_string_format_for_10_days(
+        self,
+    ):
         no_of_academic_days = 10
         expected_probability = "372/773"
         at = Attendance(no_of_academic_days)
         self.assertEqual(at.probability_to_miss_grad_ceremony(), expected_probability)
-        
+
+
 class TestInvalidNumberOfAcademicDays(unittest.TestCase):
     def test_should_raise_value_error_when_academic_days_are_less_than_1(self):
         no_of_academic_days = 0
         with self.assertRaises(ValueError) as error:
             at = Attendance(no_of_academic_days)
-            
+
         the_exception = error.exception
         self.assertEqual(the_exception.args[0], "Invalid Input")
