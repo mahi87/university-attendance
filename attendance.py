85347e3ce9846200ea498f7a45c9806a60350a80
Mahima choudhary <mahima.choudhary@jda.com>
2023-04-17 17:18:06 +0530

    refactor: test names and remove magic number

--- attendance.py
+++ attendance.py
@@ -1,3 +1,5 @@
+MAX_CONSECUTIVE_LEAVES_ALLOWED = 4
+
 class Attendance():
     def __init__(self, no_of_academic_days):
         self.no_of_academic_days = no_of_academic_days
@@ -6,12 +8,15 @@ class Attendance():
     def _number_of_ways(self, days_to_fill, consecutive_leaves_consumed):
         if (days_to_fill,consecutive_leaves_consumed) in self.cache:
             return self.cache[(days_to_fill,consecutive_leaves_consumed)]
-        if consecutive_leaves_consumed == 4:
+        
+        if consecutive_leaves_consumed == MAX_CONSECUTIVE_LEAVES_ALLOWED:
             self.cache[(days_to_fill,consecutive_leaves_consumed)] = 0
             return 0
+        
         if days_to_fill == 0:
             self.cache[(days_to_fill,consecutive_leaves_consumed)] = 1
             return 1
+        
         res = self._number_of_ways(days_to_fill-1,0) + self._number_of_ways(days_to_fill-1,consecutive_leaves_consumed+1)
         self.cache[(days_to_fill, consecutive_leaves_consumed)] = res
         return res
@@ -22,7 +27,7 @@ class Attendance():
         return self._number_of_ways(days_to_fill, consecutive_leaves_consumed)
     
     def ways_to_miss_last_day(self):
-        days_to_fill = self.no_of_academic_days -1
+        days_to_fill = self.no_of_academic_days - 1
         consecutive_leaves_consumed = 1
         return self._number_of_ways(days_to_fill, consecutive_leaves_consumed)
     
--- test_attendance.py
+++ test_attendance.py
@@ -1,38 +1,40 @@
 import unittest
 from attendance import Attendance
 
-class TestAttendance(unittest.TestCase):
-    def test_1_should_return_number_of_ways_to_attend_classes_over_n_days(self):
+class TestWaysToAttendClasses(unittest.TestCase):
+    def test_should_return_number_of_ways_to_attend_classes_over_5_days(self):
         no_of_academic_days = 5
         expected_no_of_ways = 29
         at = Attendance(no_of_academic_days)
         self.assertEqual(at.ways_to_attend_classes(), expected_no_of_ways)
         
-    def test_2_should_return_number_of_ways_to_attend_classes_over_n_days(self):
+    def test_should_return_number_of_ways_to_attend_classes_over_10_days(self):
         no_of_academic_days = 10
         expected_no_of_ways = 773
         at = Attendance(no_of_academic_days)
         self.assertEqual(at.ways_to_attend_classes(), expected_no_of_ways)
-    
-    def test_1_should_return_number_of_ways_to_miss_last_day_of_classes(self):
+
+class TestWaysToMissLastDay(unittest.TestCase):
+    def test_should_return_number_of_ways_to_miss_last_day_of_classes_for_5_days(self):
         no_of_academic_days = 5
         expected_no_of_ways = 14
         at = Attendance(no_of_academic_days)
         self.assertEqual(at.ways_to_miss_last_day(), expected_no_of_ways)
     
-    def test_2_should_return_number_of_ways_to_miss_last_day_of_classes(self):
+    def test_should_return_number_of_ways_to_miss_last_day_of_classes_for_10_days(self):
         no_of_academic_days = 10
         expected_no_of_ways = 372
         at = Attendance(no_of_academic_days)
         self.assertEqual(at.ways_to_miss_last_day(), expected_no_of_ways)
-        
-    def test_1_should_return_probability_to_miss_grad_ceremony_in_string_format(self):
+
+class TestProbabilityToMissGradCeremony(unittest.TestCase):        
+    def test_should_return_probability_to_miss_grad_ceremony_in_string_format_for_5_days(self):
         no_of_academic_days = 5
         expected_probability = "14/29"
         at = Attendance(no_of_academic_days)
         self.assertEqual(at.probability_to_miss_grad_ceremony(), expected_probability)
         
-    def test_2_should_return_probability_to_miss_grad_ceremony_in_string_format(self):
+    def test_should_return_probability_to_miss_grad_ceremony_in_string_format_for_10_days(self):
         no_of_academic_days = 10
         expected_probability = "372/773"
         at = Attendance(no_of_academic_days)
