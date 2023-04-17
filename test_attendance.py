e67b02bdba8da9c9e772faa4bbe951886fcc6e8e
Mahima choudhary <mahima.choudhary@jda.com>
2023-04-17 16:30:40 +0530

    add another test for functionality: ways to attend classes

--- test_attendance.py
+++ test_attendance.py
@@ -2,11 +2,15 @@ import unittest
 from attendance import Attendance
 
 class TestAttendance(unittest.TestCase):
-    def test_should_return_number_of_ways_to_attend_classes_over_n_days(self):
+    def test_1_should_return_number_of_ways_to_attend_classes_over_n_days(self):
         no_of_academic_days = 5
         expected_no_of_ways = 29
         at = Attendance(no_of_academic_days)
         self.assertEqual(at.ways_to_attend_classes(), expected_no_of_ways)
         
-    
+    def test_2_should_return_number_of_ways_to_attend_classes_over_n_days(self):
+        no_of_academic_days = 10
+        expected_no_of_ways = 773
+        at = Attendance(no_of_academic_days)
+        self.assertEqual(at.ways_to_attend_classes(), expected_no_of_ways)
     
